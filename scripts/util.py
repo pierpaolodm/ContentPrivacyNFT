5a347641274222d6bddc0cdb2bdb2317750de200
pierpaolo <pairogame@gmail.com>
2023-07-20 10:27:09 +0200

    input management

--- scripts/util.py
+++ scripts/util.py
@@ -1,7 +1,10 @@
 from csv import DictWriter
+import json
 import os
 import secrets
 import subprocess
+
+import numpy as np
 
 
 def measure_command(command):
@@ -40,15 +43,30 @@ def generate_circuit(info, circuit_template, id = None):
             outfile.write(circuit)
     return out_path
 
-def generate_input(output_path,full_image,low_image):
+def generate_input(output_path,full_image,low_image,commitment_key,master_keys):
     """
-    Generate a json file with the input for the circuit
-    :param output_path: path to save the json file
-    :param full_image: full image vector
-    :param low_image: low image vector
+    Generate the input for the circuit
+    :param output_path: path to the output file
+    :param full_image: full image to process
+    :param low_image: low image to process
+    :param commitment_key: commitment key that is a dictionary with the public key and the randomness, for elgamal encryption scheme
+    :param master_keys: master keys that is a list with the two master keys, for ciminion authencated encryption scheme
     """
+    json_input = {'master_key0':str(master_keys[0]),
+                  'master_key1':str(master_keys[1]),
+                  'nonce':str(generate_random_field_element()),
+                  'IV':str(generate_random_field_element()),
+                  'public_key':np.array(commitment_key['public_key']).astype(str).tolist(),
+                  'randomness':str(commitment_key['randomness'])}
+    
+    json_input['encoded_master_key0'] = elgamal_message({'message': master_keys[0], 'randomness': generate_random_field_element()}).split(',')
+    json_input['encoded_master_key1'] = elgamal_message({'message': master_keys[1], 'randomness': generate_random_field_element()}).split(',')
+
+    json_input['full_image'] = np.array(full_image).astype(str).tolist()
+    json_input['low_image'] = np.array(low_image).astype(str).tolist()
+    
     with open(output_path, 'w') as outfile:
-        outfile.write(f'{{"full_image":{full_image.tolist()},"low_image":{low_image.tolist()}}}')
+        json.dump(json_input, outfile)
 
 def parse_operation(operation):
     """
@@ -87,6 +105,26 @@ def generate_random_field_element(p=None):
     """
     if p is None:
         p = 21888242871839275222246405745257275088548364400416034343698204186575808495617
-    return secrets.randbelow(p)
+    return secrets.randbelow(p-1) + 1
     
+
+def elgamal_message(operation_values,encode = True):
+    """
+    Encode or decode a message using elgamal
+    :param operation_values: dictionary with the values to encode or decode the message, if encode is True
+                                the dictionary must contain the message and the randomness, otherwise it must
+                                contain the x, y and xIncrement values
+    :param encode: boolean to indicate if the message must be encoded or decoded
+    :return: encoded or decoded message
+    """
+    if encode:
+        operation = f'--encode {operation_values["message"]} {operation_values["randomness"]}'
+    else:
+        operation = f'--decode {operation_values["x"]} {operation_values["y"]} {operation_values["xIncrement"]}'
+
+    encoded_message = subprocess.getoutput(f'tsc ./scripts/encode_message/elgamal_message.ts && \
+                                             node ./scripts/encode_message/elgamal_message.js \
+                                             {operation}')
+    return encoded_message
+
 
